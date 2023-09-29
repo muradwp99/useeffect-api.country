d9a658e4b07ca036e8f69edd66b66b1cd15a6071
muradwp99 <muradujjaman05@gmail.com>
2023-09-29 18:32:37 +0600

    Countries Name

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,9 @@
+const Country = (props) => {
+    return (
+        <div>
+            <h3> {props.name}</h3>
+        </div>
+    );
+};
+
+export default Country;
---
+++ src/components/ExternelUsers/ExternerUsers.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+function ExternelUsers(){
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[]);
+
+
+  return (
+    <div>
+      <h1>Externel Users</h1>
+
+      {
+        users.map(user => <User key={user.id} name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+
+function User(props){
+  return (
+    <div style={{backgroundColor: '#5357FE', borderRadius: '10px', padding: '10px 0px', marginTop: '10px' }}>
+      <h3>Name: <span>{props.name}</span></h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
+export default ExternelUsers;
